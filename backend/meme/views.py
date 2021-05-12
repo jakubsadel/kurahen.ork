57151118b8ce37dd3d76fa40564f53a5819fb1af
sondisonda <kubasadel97@gmail.com>
2021-05-12 20:18:12 +0200

    Django permissions

--- backend/meme/views.py
+++ backend/meme/views.py
@@ -1,13 +1,26 @@
 from rest_framework import generics
 from meme.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAdminUser, DjangoModelPermissions, BasePermission, SAFE_METHODS
+
+
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [DjangoModelPermissions]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
