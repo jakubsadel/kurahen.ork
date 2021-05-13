ee39d85ebe5f8a39fa7493d5bcd16305aae1626d
sondisonda <kubasadel97@gmail.com>
2021-05-13 22:59:35 +0200

    Static UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+  
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+function App() {
+	const [appState, setAppState] = useState({
+		loading: false,
+		posts: null,
+	});
+
+	useEffect(() => {
+		const apiUrl = `http://127.0.0.1:8000/api/`;
+		fetch(apiUrl)
+			.then((data) => data.json())
+			.then((posts) => {
+				setAppState({ loading: false, posts: posts });
+			});
+	}, [setAppState]);
+	return (
+		<div className="App">
+			<h1>Latest Posts</h1>
+		</div>
+	);
+}
 export default App;
