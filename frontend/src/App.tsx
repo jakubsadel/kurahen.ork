294581ec9fa221fd4530b51b25d2c028df19e1e4
sondisonda <kubasadel97@gmail.com>
2021-05-14 00:54:50 +0200

    Frontend Auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,22 @@
   
-import React, { useEffect, useState } from 'react';
+import React, {  } from 'react';
 import './App.css';
 
 
-function App() {
-	const [appState, setAppState] = useState({
-		loading: false,
-		posts: null,
-	});
 
-	useEffect(() => {
+class App extends React.Component {
+	loadData() {
+
 		const apiUrl = `http://127.0.0.1:8000/api/`;
 		fetch(apiUrl)
-			.then((data) => data.json())
-			.then((posts) => {
-				setAppState({ loading: false, posts: posts });
-			});
-	}, [setAppState]);
-	return (
-		<div className="App">
-			<h1>Latest Posts</h1>
-		</div>
-	);
+		.then((response) => response.json())
+		.then((data) => console.log(data));
+	}
+	render()
+	{
+		return <div><h1>Connection</h1></div>
+	}
+
+
 }
 export default App;
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -38,16 +38,6 @@ const footers = [
 		description: ['Team', 'History', 'Contact us', 'Locations'],
 	},
 	{
-		title: 'Features',
-		description: [
-			'Cool stuff',
-			'Random feature',
-			'Team feature',
-			'Developer stuff',
-			'Another one',
-		],
-	},
-	{
 		title: 'Legal',
 		description: ['Privacy policy', 'Terms of use'],
 	},
